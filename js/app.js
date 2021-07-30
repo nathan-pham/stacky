eeb7d23fdff846cb70e1cbf5a557e4e36f949e06
Nathan Pham <nathanpham.me@gmail.com>
2021-07-30 16:32:57 -0700

    create layer

--- js/app.js
+++ js/app.js
@@ -8,10 +8,24 @@ const sketch = new Sketch({container: "#webgl__container"})
 
 sketch.scene.background = new THREE.Color(0x00000)
 
+const layer = (x, z, width, depth, direction) => {
+    const blocks = sketch.objects.filter(object => object?.type == "block")
+    const height = 1
+    const y = height * blocks.length
+
+    const color = new THREE.Color(`hsl(${30 + blocks.length * 4}, 100%, 50%)`)
+    const block = new Block({size: [width, height, depth], position: [x, y, z], color})
+    block.direction = direction
+
+    return block
+}
+
 sketch.add(
-    new Block([3, 1, 3]),
     new AmbientLight(),
     new DirectionalLight()
 )
+
+sketch.add(layer(0, 0, 3, 3, "x"))
+sketch.add(layer(0, 0, 3, 3, "z"))
 
 sketch.render()
--- js/classes/objects/Block.js
+++ js/classes/objects/Block.js
@@ -1,11 +1,15 @@
 import * as THREE from "https://esm.sh/three"
 
 export default class Block {
-    constructor(size=[1, 1, 1]) {
+    type = "block"
+    direction = "x"
+
+    constructor({size=[1, 1, 1], position=[0, 0, 0], color=0xfb8e00}={}) {
         this.geometry = new THREE.BoxGeometry(...size)
-        this.material = new THREE.MeshLambertMaterial({color: 0xfb8e00})
+        this.material = new THREE.MeshLambertMaterial({color})
 
         const mesh = new THREE.Mesh(this.geometry, this.material)
+        mesh.position.set(...position)
         mesh.receviveShadow = true
         mesh.castShadow = true
 
